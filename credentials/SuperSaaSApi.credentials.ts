a1b5ca72f89fecbdddcc9eac29811d03128e9ae6
Kaarle Kulvik <kaarle@supersaas.com>
2024-06-19 16:23:00 +0200

    Small refactoring

--- credentials/SuperSaaSApi.credentials.ts
+++ credentials/SuperSaaSApi.credentials.ts
@@ -1,14 +1,8 @@
 import {
-	IAuthenticateGeneric,
 	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
 } from 'n8n-workflow';
-
-//const BASE_URL = 'https://www.supersaas.com';
-//const BASE_URL = 'https://05df-89-205-225-51.ngrok-free.app';
-const BASE_URL = 'http://localhost:3000'
-
 
 export class SuperSaaSApi implements ICredentialType {
 	name = 'superSaaSApi';
@@ -28,25 +22,14 @@ export class SuperSaaSApi implements ICredentialType {
 			default: '',
 		}
 	];
-
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			qs: {
-				account: '={{$credentials.account}}',
-				api_key: '={{$credentials.api_key}}',
-			},
-		},
-	};
-
 	test: ICredentialTestRequest = {
 		request: {
-				baseURL: BASE_URL,
+				baseURL: 'https://www.supersaas.com',
 				url: '/api/ping',
 				method: 'GET',
-				// qs: {
-				// 	account: '={{$credentials.account}}',
-				// },
+				qs: {
+					account: '={{$credentials.account}}',
+				},
 				headers: {
 					Authorization: '=Bearer {{$credentials.api_key}}',
 				},
